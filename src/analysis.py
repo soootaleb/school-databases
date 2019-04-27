e91c9a8e4dadc8f03f7be29ba5507dbb20094d41
Paris Descartes <Descartes@Sofianes-MacBook-Pro.local>
2019-04-27 12:51:26 +0200

    Added same_circle util

--- src/analysis.py
+++ src/analysis.py
@@ -1,3 +1,24 @@
 from loader import USERS, SCORES
+from constants import DATA_DIR
 
-print('OK')
+import os
+
+'''
+Returns True if the two users are in a circle
+Returns False otherwise
+'''
+def same_circle(left, right):
+    with open(os.path.join(DATA_DIR, left + '.circles'), 'r', encoding='utf8') as circles:
+        for circle in filter(len, circles.read().split('\n')):
+            users = circle.split('\t')[1:]
+            if right in users:
+                return True
+        return False
+
+
+if __name__ == "__main__":
+
+    same_circle('532617990', '202674661') # True
+    same_circle('532617990', '154070583') # False
+
+    print('OK')
