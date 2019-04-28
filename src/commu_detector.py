e29dc098a03e46100d832285cc4435e494cef8ce
tan0407 <45198471+tan0407@users.noreply.github.com>
2019-04-28 18:23:10 +0200

    Removed an algorithm (due to bad results)

--- src/commu_detector.py
+++ src/commu_detector.py
@@ -1,7 +1,7 @@
 import json,os
 
-from analysis import same_circle
-from constants import BASE_DIR, EVAL_COMMU_DETECTOR_MESSAGE
+from analysis import same_circle,same_circle_one
+from constants import BASE_DIR, EVAL_COMMU_DETECTOR_MESSAGE,EVAL_COMMU_DETECTOR2_MESSAGE
 
 with open(os.path.join(BASE_DIR, 'scoring_alpha.output.json'), 'r') as input:
     USERS = json.loads(input.read())
@@ -9,9 +9,10 @@ with open(os.path.join(BASE_DIR, 'scoring_alpha.output.json'), 'r') as input:
 followsThreshold = 80
 hashtagThreshold = 15
 mentionThreshold = 7
-community = []
+
 
 def detect():
+    community = []
     user= set()
     for link in USERS:
         usa, usb = link.split('-')
@@ -29,8 +30,18 @@ def detect():
     print ("Community : ", community)
     print ("Size of the community : ",len(community))
     print("out of ", len(user), "with in total", len(USERS))
+    counter = 0
+    for com in community:
+        left, right = com.split('-')
+        counter = counter + 1 if same_circle(left, right) else counter
+
+    print(EVAL_COMMU_DETECTOR_MESSAGE.format(len(community), counter))
+
 if __name__ == "__main__":
     detect()
+
+'''
+THIS FUNCTION IS NO LONGER USED DUE TO BAD RESULTS
 
 # Detects a community by comparing all the users between them 
 def detect_complex_commu():
@@ -50,15 +61,15 @@ def detect_complex_commu():
                 communities.append(community)
     for commu in communities:
         print(commu)
+    counter = 0
+    for community in communities:
+        for id in community:
+            counter = counter + 1 if same_circle_one(id) else counter
+    print(EVAL_COMMU_DETECTOR2_MESSAGE.format(len(community), counter))
 
 if __name__ == "__main__":
     detect_complex_commu()
     
-    '''
-    counter = 0
-    for com in community:
-        left, right = com.split('-')
-        counter = counter + 1 if same_circle(left, right) else counter
-
-    print(EVAL_COMMU_DETECTOR_MESSAGE.format(len(community), counter))
-    '''
+'''    
+    
+    
